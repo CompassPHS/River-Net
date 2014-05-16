72e7722be1c96644a9530feedd5cd2a100b7ca48
Jared Ashman <jared.ashman@compassphs.com>
2014-05-16 09:36:50 -0500

    Revert "Intial TPL dataflow implementation for async operation"
    This reverts commit 0822079c8e8d0755a5234d4f3d71970858051878.
    
    Revert async - memory consumption out of control

--- River.Components/River.cs
+++ River.Components/River.cs
@@ -11,7 +11,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 
 namespace River.Components
 {
@@ -21,56 +20,109 @@ namespace River.Components
         Sources.Source _source;
         Mouth _mouth;
 
-        /// <summary>
-        /// Links the Source of the River and the Mouth of the River.
-        /// </summary>
-        TransformBlock<
-            Dictionary<string, object>,
-            Dictionary<string, object>> Bed { get; set; }
-
         ILog log = Common.Logging.LogManager.GetCurrentClassLogger();
 
         public River(RiverContext riverContext)
         {
             _riverContext = riverContext;
-            CreateFlow();
-
-            _source = Sources.Source.GetSource(riverContext.Source, Bed);
-            _mouth = new Mouth(riverContext.Destination, Bed);
-        }
-
-        private void CreateFlow()
-        {
-            Bed = new TransformBlock<Dictionary<string, object>, Dictionary<string, object>>(bucket =>
-            {
-                return bucket;
-            });
+            _source = Sources.Source.GetSource(riverContext.Source);
+            _mouth = new Mouth(riverContext.Destination);            
         }
 
         public void Flow()
         {
             log.Info(string.Format("Starting river {0}", _riverContext.Name));
+            Dictionary<string, object> curObj = null;
 
             try
             {
-                var start = System.Environment.TickCount;
-                foreach (var bucket in _source.GetWater())
+                foreach (var rowObj in _source.GetRows(_riverContext.Source))
                 {
-                    _source.Pull(bucket);
-                }
+                    try
+                    {
+                        if (curObj == null)
+                        {
+                            curObj = new Dictionary<string, object>();
+                        }
+                        else if (!curObj.ContainsKey("_id") || curObj["_id"].ToString() != rowObj["_id"].ToString())
+                        {
+                            //push curObj
+                            _mouth.PushObj(curObj, false);
 
-                _source.Empty();
-                _mouth.IsEmptied();
-                var end = System.Environment.TickCount;
-                log.Info(string.Format("River took {0}", (end - start) / 1000));
+                            //now make a new obj
+                            curObj = new Dictionary<string, object>();
+                        }
+
+                        Merge(rowObj, curObj);
+                    }
+                    catch (Exception e)
+                    {
+                        log.Error(string.Format("Error river {0}", _riverContext.Name), e);
+                    }
+                }
             }
             catch (Exception e)
-            {
+            {                
                 log.Error(string.Format("Error river {0}", _riverContext.Name), e);
             }
 
+            if (curObj != null) _mouth.PushObj(curObj, true);
+
+            _mouth.Empty();
             log.Info(string.Format("Completed river {0}", _riverContext.Name));
-            
+        }
+
+
+        private void Merge(Dictionary<string, object> src, Dictionary<string, object> dest)
+        {
+            foreach (var skvp in src)
+            {
+                if (!dest.ContainsKey(skvp.Key))
+                {
+                    dest.Add(skvp.Key, skvp.Value);
+                }
+                else if (skvp.Value.GetType() == typeof(Dictionary<string, object>))
+                {
+                    Merge(skvp.Value as Dictionary<string, object>, dest[skvp.Key] as Dictionary<string, object>);
+                }
+                else if (skvp.Value.GetType() == typeof(List<Dictionary<string, object>>))
+                {
+                    var srcList = skvp.Value as List<Dictionary<string, object>>;
+                    var destList = dest[skvp.Key] as List<Dictionary<string, object>>;
+
+                    foreach (var srcChild in srcList)
+                    {
+                        Dictionary<string, object> destMatch = null;
+                        foreach (var destChild in destList)
+                        {
+                            if (destChild.ContainsKey("_id") && srcChild.ContainsKey("_id")
+                                && destChild["_id"].ToString() == srcChild["_id"].ToString())
+                            {
+                                destMatch = destChild;
+                                break;
+                            }
+                        }
+
+                        if (destMatch != null) Merge(srcChild, destMatch);
+                        else destList.Add(srcChild);
+                    }
+                }
+                else if (skvp.Value.GetType() == typeof(List<object>))
+                {
+                    var srcList = skvp.Value as List<object>;
+                    var destList = dest[skvp.Key] as List<object>;
+
+                    foreach (var srcChild in srcList)
+                    {
+                        if (!destList.Contains(srcChild))
+                            destList.Add(srcChild);
+                    }
+                }
+                else
+                {
+                    dest[skvp.Key] = skvp.Value;
+                }
+            }
         }
     }
 }
